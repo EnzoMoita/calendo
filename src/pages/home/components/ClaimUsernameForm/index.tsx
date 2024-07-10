23029c9651ca094aa5841f1ce63da48cd2b8eaee
Enzo Salomão <enzosadourado@hotmail.com>
2024-07-10 08:02:10 -0300

    firtPageWithDesignSystemIgnite

---
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -0,0 +1,16 @@
+import { Button, TextInput } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { Form } from './styles'
+
+export function ClaimUsernameForm() {
+  return (
+    <Form as="form">
+    {/* @ts-ignore */}
+      <TextInput size="sm" prefix="calendo.com/" placeholder="seu-usuário" />
+      <Button size="sm" type="submit">
+        Reservar
+        <ArrowRight />
+      </Button>
+    </Form>
+  )
+}
