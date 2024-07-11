f5c01536db9bf82ffc36308e70bd3843d7c816bb
Enzo Salomão <enzosadourado@hotmail.com>
2024-07-11 19:03:43 -0300

    Createusers

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,24 +1,29 @@
 import { Button, Text, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
-import { Form, FormAnnotation } from './styles'
 import { useForm } from 'react-hook-form'
-import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import { Form, FormAnnotation } from './styles'
 import { useRouter } from 'next/router'
 
 const ClaimUsernameFormSchema = z.object({
-  username: z.string()
-  .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
-  .regex(/^([a-z\\-]+)$/i, {
-    message: 'O usuário pode ter apenas letras e hifens.',
-  })
-  .transform((username) => username.toLowerCase()),
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hifens.',
+    })
+    .transform((username) => username.toLowerCase()),
 })
 
 type ClaimUsernameFormData = z.infer<typeof ClaimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
-  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<ClaimUsernameFormData>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ClaimUsernameFormData>({
     resolver: zodResolver(ClaimUsernameFormSchema),
   })
 
@@ -27,28 +32,32 @@ export function ClaimUsernameForm() {
   async function handleClaimUsername(data: ClaimUsernameFormData) {
     const { username } = data
 
-    await router.push(`/register?username=${username}`)
+    router.push(`/register?username=${username}`)
   }
-
 
   return (
     <>
-    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
-    {/* @ts-ignore */}
-      <TextInput size="sm" prefix="calendo.com/" placeholder="seu-usuário" {...register('username')} />
-      <Button size="sm" type="submit" disable={isSubmitting} >
-        Reservar
-        <ArrowRight />
-      </Button>
-    </Form>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+            {/* @ts-ignore */}
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu-usuário"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit" disabled={isSubmitting}>
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
 
-    <FormAnnotation>
-      <Text size="sm">
-        {errors.username
-        ? errors.username.message
-        : 'Digite o nome do usuário desejado'}
-      </Text>
-    </FormAnnotation>
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : 'Digite o nome do usuário desejado'}
+        </Text>
+      </FormAnnotation>
     </>
   )
 }
